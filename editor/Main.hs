60fb98691d101b63aeba577d7b278b960c3b11ca
Alexander Goussas <goussasalexander@gmail.com>
2022-09-28 01:29:42 -0500

    started menu bar

--- editor/Main.hs
+++ editor/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
+import qualified Mal.Editor.Gui.MenuBar    as MenuBar
 import qualified Mal.Editor.Gui.TextEditor as TextEditor
 import qualified Mal.Editor.Gui.Toolbar    as Toolbar
 
 import qualified Mal
 
+import           Control.Monad             (forM_, void)
 import           Data.Text                 (Text)
 import qualified GI.Gio                    as Gio
 import qualified GI.Gtk                    as Gtk
@@ -27,30 +29,40 @@ run app = do
   Gtk.setWindowDefaultHeight win 600
 
   layout <- Gtk.boxNew Gtk.OrientationVertical 10
+
   textEditor <- TextEditor.empty
   toolbar <- createToolBar textEditor
+  menuBar <- MenuBar.new
 
+  Gtk.boxAppend layout menuBar
   Gtk.boxAppend layout toolbar
   Gtk.boxAppend layout textEditor
 
   Gtk.windowSetChild win $ Just layout
 
   Gtk.widgetShow textEditor
-  _ <- Gtk.widgetGrabFocus textEditor
+  void $ Gtk.widgetGrabFocus textEditor
 
+  void $ createApplicationActions app textEditor
   Gtk.widgetShow win
 
+createApplicationActions :: Gtk.Application -> TextEditor.TextEditor -> IO ()
+createApplicationActions app textEditor = do
+  quitAction <- Gio.simpleActionNew "quit" Nothing
+  void $ Gio.onSimpleActionActivate quitAction (const $ Gio.applicationQuit app)
+
+  runCodeAction <- Gio.simpleActionNew "run-code" Nothing
+  void $ Gio.onSimpleActionActivate runCodeAction (const $ runMal textEditor)
+
+  forM_ [quitAction , runCodeAction] (Gio.actionMapAddAction app)
 
 createToolBar :: TextEditor.TextEditor -> IO Toolbar.ToolBar
-createToolBar textEditor = Toolbar.new runAction
-  where
-    runAction = do
-      program <- TextEditor.getContents textEditor
-      result <- runMal program
-      print result
+createToolBar textEditor = Toolbar.new (runMal textEditor)
 
 -- TODO: pass in a filename
 -- TODO: handle exceptions
-runMal :: Text -> IO Text
-runMal = do
-  Mal.runOnce Nothing
+runMal :: TextEditor.TextEditor -> IO ()
+runMal textEditor = do
+  program <- TextEditor.getContents textEditor
+  result <- Mal.runOnce Nothing program
+  print result
---
+++ editor/Mal/Editor/Gui/MenuBar.hs
@@ -0,0 +1,47 @@
+module Mal.Editor.Gui.MenuBar
+(
+  new
+)
+where
+
+import qualified GI.Gio as Gio
+import qualified GI.Gtk as Gtk
+
+
+type MenuBar = Gtk.PopoverMenuBar
+
+new :: IO MenuBar
+new = do
+  menu <- Gio.menuNew
+
+  fileMenu <- createFileMenu
+  helpMenu <- createHelpMenu
+  runMenu <- createRunMenu
+
+  Gio.menuAppendSubmenu menu (Just "File") fileMenu
+  Gio.menuAppendSubmenu menu (Just "Run") runMenu
+  Gio.menuAppendSubmenu menu (Just "Help") helpMenu
+
+  Gtk.popoverMenuBarNewFromModel (Just menu)
+
+createRunMenu :: IO Gio.Menu
+createRunMenu = do
+  runMenu <- Gio.menuNew
+  Gio.menuAppend runMenu (Just "Run") (Just "app.run-code")
+  pure runMenu
+
+createHelpMenu :: IO Gio.Menu
+createHelpMenu = do
+  helpMenu <- Gio.menuNew
+  aboutItem <- Gio.menuItemNew (Just "About") Nothing
+  Gio.menuAppendItem helpMenu aboutItem
+  pure helpMenu
+
+createFileMenu :: IO Gio.Menu
+createFileMenu = do
+  fileMenu <- Gio.menuNew
+  Gio.menuAppend fileMenu (Just "Open File") Nothing
+  Gio.menuAppend fileMenu (Just "New file") Nothing
+  Gio.menuAppend fileMenu (Just "Save") Nothing
+  Gio.menuAppend fileMenu (Just "Exit") (Just "app.quit")
+  pure fileMenu
